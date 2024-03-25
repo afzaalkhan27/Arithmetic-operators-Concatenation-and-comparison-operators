4aae3d616895010c2fd3b36d501a80f2a215bbca
Afzaal Khan <162747532+afzaalkhan27@users.noreply.github.com>
2024-03-26 02:46:46 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,434 @@
+//Addition [+]   (START)
+
+let n1: number =  10;
+let n2: number = 20;
+console.log(n1 + n2)
+
+  const num1 = 8;
+  const num2 = 4;
+  console.log(num1 + num2);
+
+ const teachers = 10;
+ const  students = 50;
+ console.log(teachers + students);
+
+  const myaccountblance1 = 50000;
+  const myaccountblance2 = 30000;
+ console.log(myaccountblance1 + myaccountblance2);
+
+ const brothers = 5;
+const sisters = 2;
+ console.log(brothers + sisters);
+
+const apple = 12;
+const greenApple = 11;
+ console.log(apple + greenApple);
+
+ const candy = 50;
+ const chocolate =100;
+console.log(candy + chocolate);
+
+ const pakteam = 45
+ const indteam = 23
+
+ const sum = pakteam + indteam
+
+ console.log(sum);
+
+  const kidsinclass1 = 30;
+ const kidsinclass2 = 50;
+
+ const sum = kidsinclass1 + kidsinclass2
+
+ console.log(sum);
+
+ const popleinmall1 = 1000;
+ const popleinmall2 = 3000;
+
+ const sum = popleinmall1 + popleinmall2
+ console.log(sum);
+
+ const guest_in_hall_A = 1500;
+ const guest_in_hall_B = 1000;
+ const sum = guest_in_hall_A + guest_in_hall_B
+   console.log(sum);
+
+ const mango_on_tree_1 = 500;
+ const mango_on_tree_2 = 700;
+ console.log(mango_on_tree_1 + mango_on_tree_2);
+
+  const price_of_oli_per_kg_1 = 780;
+ const price_of_oli_per_kg_2 = 960;
+ console.log(price_of_oli_per_kg_1 + price_of_oli_per_kg_2)
+ 
+ const car_in_raw_1 = 70;
+const car_in_raw_2 = 50;
+console.log(car_in_raw_1 + car_in_raw_2);
+
+ const total_seats_in_bus_1 = 72;
+ const total_seats_in_bus_2 = 35;
+   console.log(car_in_raw_1 + car_in_raw_2);
+
+//                      //END
+
+
+//   //subtraction [-]    (START)
+
+const num1 = 8;
+const num2 = 4;
+console.log(num1 - num2);
+ 
+ 
+const pricofoilperkg1 = 3000;
+const priceofoilperkg2 = 500;
+console.log(priceofoilperkg1 - priceofoilperkg2);
+
+
+const guest_in_hall_A = 1500;
+const guest_in_hall_B = 1000;
+const subtraction = guest_in_hall_A - guest_in_hall_B
+console.log(subtraction);
+
+
+const mango_on_tree_1 = 1000;
+const mango_on_tree_2 = 700;
+console.log(mango_on_tree_1 - mango_on_tree_2);
+
+
+const guest_in_hall_A = 1500;
+const guest_in_hall_B = 1000;
+const subtraction = guest_in_hall_A - guest_in_hall_B
+console.log(subtraction);
+
+
+const chairs_in_class_1  = 75;
+const chairs_in_class_2 = 50;
+console.log(chairs_in_class_1 - chairs_in_class_2);
+ 
+
+const Book_on_table_1 = 45;
+const Book_on_table_2 = 30;
+console.log(Book_on_table_1 - Book_on_table_2);
+
+
+const total_seats_in_bus_1 = 72;
+const total_seats_in_bus_2 = 35;
+const subtraction = total_seats_in_bus_1 - total_seats_in_bus_2
+console.log(subtraction);
+
+const banana_on_basket_1 = 15;
+const banana_on_basket_2 = 10;
+console.log(banana_on_basket_1 - banana_on_basket_2);
+
+
+const laptop_on_table_1 = 3;
+const laptop_on_table_2 = 2;
+console.log(laptop_on_table_1 - laptop_on_table_2);
+
+                  //   END
+
+
+
+//multiplication[*]     (START)
+
+
+const Book_on_table_1 = 50;
+const Book_on_table_2 = 10;
+console.log(Book_on_table_1 * Book_on_table_2);
+
+
+
+const total_seats_in_bus_1 = 100;
+const total_seats_in_bus_2 = 60;
+const multiplication = total_seats_in_bus_1 * total_seats_in_bus_2
+console.log(multiplication);
+
+
+const price_of_oli_per_kg_1 = 30000;
+const price_of_oli_per_kg_2 = 500;
+console.log(price_of_oli_per_kg_1 * price_of_oli_per_kg_2);
+
+
+const laptop_on_table_1 = 30;
+const laptop_on_table_2 = 20;
+console.log(laptop_on_table_1  *  laptop_on_table_2);
+
+
+const lights_on_room_1 = 10;
+const lights_on_room_2 = 8;
+const mul =lights_on_room_1 * lights_on_room_2
+console.log(mul);
+
+const chairs_in_class_1  = 70;
+const chairs_in_class_2 = 50;
+console.log(chairs_in_class_1 *  chairs_in_class_2);
+ 
+
+ const glass_on_tray_1 = 6;
+ const glass_on_tray_2 = 4;
+ console.log(glass_on_tray_1 * glass_on_tray_2);
+
+const shift_one_students = 2000;
+const shift_two_students = 1500;
+console.log(shift_one_students * shift_two_students);
+
+
+const rooms_on_guesthouse_1 = 55;
+const rooms_on_guesthouse_2 = 45;
+const multiplication = rooms_on_guesthouse_1 * rooms_on_guesthouse_2
+console.log(multiplication);
+
+const children_on_school_bus_1 = 45;
+const children_on_school_bus_2 = 25;
+ console.log(children_on_school_bus_1 * children_on_school_bus_2);
+
+                      //END
+
+
+
+ //Division [/]     (START)
+
+const Book_on_table_1 = 50;
+const Book_on_table_2 = 10;
+console.log(Book_on_table_1 / Book_on_table_2);
+
+const shift_one_students = 200;
+const shift_two_students = 100;
+console.log(shift_one_students / shift_two_students);
+
+const banana_on_basket_1 = 50;
+const banana_on_basket_2 = 10;
+console.log(banana_on_basket_1 / banana_on_basket_2);
+
+const chairs_in_class_1  = 75;
+const chairs_in_class_2 = 50;
+console.log(chairs_in_class_1 / chairs_in_class_2);
+
+const lifts_in_mall_1 = 6;
+const lifts_in_mall_2 = 6;
+console.log(lifts_in_mall_1 / lifts_in_mall_2);
+ 
+const rooms_on_guesthouse_1 = 90;
+const rooms_on_guesthouse_2 = 55;
+const Division = rooms_on_guesthouse_1 / rooms_on_guesthouse_2
+console.log(Division);
+
+const players_in_team_pakistan = 11;
+const players_in_team_india = 10;
+const div = players_in_team_pakistan / players_in_team_india
+console.log(div);
+
+const people_in_Red_bus = 80;
+const people_in_green_bus = 70;
+const div = people_in_Red_bus / people_in_green_bus
+console.log(div);
+
+const cars_on_flyover_1 = 200;
+const cars_on_flyover_2 = 100;
+console.log(cars_on_flyover_1 / cars_on_flyover_2);
+
+
+const cow_on_truck_1 = 500;
+const cow_on_truck_2 = 300;
+console.log(cow_on_truck_1 / cow_on_truck_2);  
+
+//  //Exponentiation [**] (power)
+
+const chairs_in_class_1  = 50;
+const chairs_in_class_2 = 10;
+const power = chairs_in_class_1 ** chairs_in_class_2;
+console.log(power);
+
+const layer = 4
+const apple = 4
+const power = layer ** apple
+console.log(power)
+
+
+const cat = 3
+const loin = 2
+const power = cat ** loin
+console.log(power)
+
+const pakistan = 8
+const india = 4
+console.log(pakistan ** india);
+
+const red = 4
+const green = 2
+const power = red ** green
+console.log(power)
+ 
+const islamabad  = 4
+const karachi = 2
+console.log(islamabad ** karachi)
+
+const  guestInhalla= 60
+const guestInhallb = 10
+const power = guestInhalla ** guestInhallb
+console.log(power)
+
+const ball = 2
+const basket_ball = 2
+console.log(ball **  basket_ball)
+
+const japan = 5
+const island = 3
+const power = japan ** island
+console.log(power)
+
+const shift_one_students = 10
+const shift_two_students = 15
+
+console.log(shift_one_students ** shift_two_students)
+
+//                     END
+
+
+
+//  //Module [%]     (START)
+
+const pencil = 20
+const students = 10
+console.log(pencil % students)
+
+const price_of_book = 500
+const  quantity_of_books = 10
+const Module = price_of_book % quantity_of_books
+console.log(Module)
+
+const number_of_hours = 30
+const  salary_per_hour = 70
+console.log(number_of_hours % salary_per_hour)
+
+const apple = 10
+const bags = 2
+const reminder = apple % bags;
+console.log(reminder)
+
+const guest_in_room = 20
+const cup_of_tea = 10
+console.log(guest_in_room % cup_of_tea);
+                        
+//                END
+
+
+//  CONCATENATION     (START)
+
+const myName = "AFZAAL"
+const fatherName = "BASHIR AHMED"
+console.log(myName + " " + fatherName)
+
+const  city = "karachi"
+const specialty = "city of lights"
+console.log(city + " " + specialty );
+
+const myGrandFather = "AHMED"
+const lastName = "ALI KHAN"
+console.log(myGrandFather + " " + lastName );
+
+const myLove = "MY MOTHER"
+const myWorld = "MY WORLD"
+console.log(myLove + " " + myWorld);
+                                      
+//                 //END
+
+
+
+// // (START)
+
+let x = 15
+x += 5
+console.log(x)
+
+let b = 20
+b = b + 5
+console.log(b)
+
+
+let z = 50
+z = z + 20
+console.log(z)
+
+let a = 10
+a = a + 20
+console.log(a)
+
+let c = 40
+c += 20
+console.log(c);
+
+let oranges = 5
+oranges += 10
+console.log(oranges);
+
+let ball = 70
+ball -= 20
+console.log(ball);
+
+
+let pen = 30
+pen = pen + 70
+console.log(pen);
+
+let mango = 200
+mango = mango - 50
+console.log(mango);
+
+let tree = 400
+tree = tree + 600
+console.log(tree);
+                         
+
+
+// comparison operator   (START)
+
+console.log( 5 == 5);
+console.log(7 != 7 ); 
+console.log(2 > 1);
+console.log(3 < 7);
+console.log(6 >= 4); 
+
+console.log( 5 == 6);
+console.log(7 != 2); 
+console.log(6 > 1);
+console.log(6 < 7);
+console.log(7 >= 4); 
+
+console.log(10 == 10);
+console.log(30 != 20);
+console.log(15 > 10);
+console.log(11 < 12);
+console.log(20 >= 5);
+
+console.log(8 == 8);     // is_Equal
+console.log(3 != 5 );     //is_not_Equal
+console.log(6 > 4);      //is_greater_than
+console.log(2 < 1);      // is_less_than
+
+
+console.log(10 == "10abc");  // ==
+console.log(10 === "10"); //===
+console.log(10 != 5); // !=
+console.log(5 > 2); //>
+console.log(5 < 2); //<
+console.log(5 <= 5); // <
+console.log(2 <= 5); // <=
+console.log(2 >= 5); //>=
+
+console.log(4 == 4);
+console.log(80 != 50);
+console.log(5 > 4);
+console.log(5 < 4);
+console.log(3 <= 3);
+console.log(1 <= 5);
+console.log(3 >= 5);
+
+   // (THE-END!)
+
+
+ 
+
+
+
+
